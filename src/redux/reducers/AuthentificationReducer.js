4a9b4b78ee5095147d6f0abd68a87cc070c52f15
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-06 21:16:59 +0200

    Add authentification reducer

---
+++ src/redux/reducers/AuthentificationReducer.js
@@ -0,0 +1,27 @@
+import Constants from '../../helpers/Constants';
+
+const user = JSON.parse(localStorage.getItem('user'));
+const initialState = user ? { loggedIn: true, user } : {};
+
+const authentication = (state = initialState, action) => {
+  switch (action.type) {
+    case Constants.LOGIN_REQUEST:
+      return {
+        loggingIn: true,
+        user: action.user,
+      };
+    case Constants.LOGIN_SUCCESS:
+      return {
+        loggedIn: true,
+        user: action.user,
+      };
+    case Constants.LOGIN_FAILURE:
+      return {};
+    case Constants.LOGOUT:
+      return {};
+    default:
+      return state;
+  }
+};
+
+export default authentication;
