4f5430053757f6fbe6f0fdb92d9dffbacdd82548
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-06 16:18:42 +0200

    Setup routes

---
+++ src/screens/App.js
@@ -0,0 +1,13 @@
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import Main from '../components/Main';
+
+const App = () => (
+  <>
+  <Header />
+  <Main />
+  <Footer />
+</>
+);
+
+export default App;
