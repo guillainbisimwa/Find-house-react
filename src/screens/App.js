ee82ea3f7544e364e0329c5076534f7e5dbeb547
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-06 23:08:25 +0200

    Display username in the homepage is the user is logged in

--- src/screens/App.js
+++ src/screens/App.js
@@ -1,13 +1,26 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import Main from '../components/Main';
 
-const App = () => (
-  <>
-  <Header />
-  <Main />
-  <Footer />
-</>
-);
+const App = () => {
+  const user = useSelector((state) => state.authentication.user);
+
+  return (
+    <>
+    <Header />
+      <div className="col-lg-8 offset-lg-2">
+        <h1>Hi {user.firstName}!</h1>
+        <p>You are logged in with React Hooks!!</p>
+        <p>
+          <Link to="/login">Logout</Link>
+        </p>
+      </div>
+    <Main />
+    <Footer />
+  </>
+  );
+};
 
 export default App;
