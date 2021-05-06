979dec4c93f1cbe2728a0b8df4db699ec5982ce1
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-06 19:14:07 +0200

    Allow the API key to be include in all API requests to the server in a header

---
+++ src/helpers/AuthHeader.js
@@ -0,0 +1,11 @@
+const authHeader = () => {
+  // return authorization header with jwt token
+  const user = JSON.parse(localStorage.getItem('user'));
+
+  if (user && user.token) {
+    return { Authorization: `Bearer ${user.auth_token}` };
+  }
+  return {};
+};
+
+export default authHeader;
