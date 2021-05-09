6ef794e9b5a97ac2518bafd928463c63897f92e5
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-09 22:06:18 +0200

    Add favorite's houses reducer

---
+++ src/redux/reducers/FavoriteReducer.js
@@ -0,0 +1,22 @@
+import Constants from '../../helpers/Constants';
+
+const favorite = (state = {}, action) => {
+  switch (action.type) {
+    case Constants.GETALLFAVORITES_REQUEST:
+      return {
+        loading: true,
+      };
+    case Constants.GETALLFAVORITES_SUCCESS:
+      return {
+        favorites: action.favorite,
+      };
+    case Constants.GETALLFAVORITES_FAILURE:
+      return {
+        error: action.error,
+      };
+    default:
+      return state;
+  }
+};
+
+export default favorite;
