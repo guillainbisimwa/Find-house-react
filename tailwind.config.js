a9d5c932e34fe81ceb5309ee1fd2e1c586f74574
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-10 15:20:57 +0200

    Style login screen

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,21 @@ module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: () => ({
+        'login-bg': "url('/src/assets/bg.jpg')",
+      }),
+      backgroundColor: theme => ({
+        ...theme('colors'),
+        primary: '#ec5200',
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    // eslint-disable-next-line global-require
+    require('@tailwindcss/forms'),
+  ],
 };
