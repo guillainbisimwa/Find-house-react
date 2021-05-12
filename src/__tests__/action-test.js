62c3d02a28e80600d85a54eb9e02b45299300ebe
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-12 21:40:21 +0200

    Test house action

--- src/__tests__/action-test.js
+++ src/__tests__/action-test.js
@@ -12,6 +12,29 @@ const expectedLoginState = {
   id: '1',
 };
 
+const expectedHouseState = [
+  {
+    id: 4,
+    price: 200.0,
+    details: 'details 1',
+    about: 'A single house',
+    picture: 'https://link.jpeg',
+    owner: 'Autjer',
+    created_at: '2021-05-03',
+    updated_at: '2021-05-03',
+  },
+  {
+    id: 5,
+    price: 800.0,
+    details: 'details',
+    about: 'A single house',
+    picture: 'https://link.jpeg',
+    owner: 'Autjer',
+    created_at: '2021-05-03',
+    updated_at: '2021-05-03',
+  },
+];
+
 const registerObject = {
   name: 'test',
   email: 'test@me.com',
@@ -40,14 +63,28 @@ const signUpApiUrl = {
   data: registerObject,
 };
 
+const getAllHouseApiUrl = {
+  method: 'GET',
+  url: 'https://find-your-house-backend.herokuapp.com/houses',
+  params: {},
+  headers: {
+    authorization: '********',
+  },
+};
+
 const successRegister = () => ({
   type: 'USERS_REGISTER_SUCCESS',
   user: expectedRegisterState,
 });
 
 const successLogin = () => ({
-  type: 'USERS_REGISTER_SUCCESS',
+  type: 'USERS_LOGIN_SUCCESS',
   user: expectedRegisterState,
+});
+
+const successHouses = () => ({
+  type: 'GETALLHOUSES_SUCCESS',
+  houses: expectedHouseState,
 });
 
 const fetchDataRegister = async () => dispatch => fetch(('https://find-your-house-backend.herokuapp.com/signup', {
@@ -63,6 +100,13 @@ const fetchDataLogin = async () => dispatch => fetch(('https://find-your-house-b
   headers: { 'content-type': 'application/json' },
 }))
   .then(() => dispatch(successLogin()));
+
+const fetchDataHouse = async () => dispatch => fetch(('https://find-your-house-backend.herokuapp.com/houses', {
+  data: registerObject,
+  method: 'POST',
+  headers: { 'content-type': 'application/json', authorization: '********' },
+}))
+  .then(() => dispatch(successHouses()));
 
 describe('sign up', () => {
   beforeEach(() => {
@@ -131,5 +175,45 @@ describe('login', () => {
   it('should not return an empty array', async () => store.dispatch(fetchDataLogin)
     .then(() => {
       expect(store.getState()).not.toEqual([]);
+    }));
+});
+
+describe('houses', () => {
+  beforeEach(() => {
+    moxios.install();
+  });
+  afterEach(() => {
+    moxios.uninstall();
+  });
+
+  const store = mockStore(expectedHouseState);
+
+  it('Store is updated correctly', async () => {
+    moxios.wait(() => {
+      moxios.stubOnce('GET', getAllHouseApiUrl, {
+        status: 200,
+        response: expectedHouseState,
+      });
+    });
+
+    return store.dispatch(fetchDataHouse).then(() => {
+      const newState = store.getState();
+      expect(Object.entries(newState)).toEqual(Object.entries(expectedHouseState));
+    });
+  });
+
+  it('should return an empty array', async () => store.dispatch(fetchDataHouse)
+    .then(() => {
+      expect(store.getActions()).toEqual([]);
+    }));
+
+  it('should not return an empty array', async () => store.dispatch(fetchDataHouse)
+    .then(() => {
+      expect(store.getState()).not.toEqual([]);
+    }));
+
+  it('should not return the size of the object', async () => store.dispatch(fetchDataHouse)
+    .then(() => {
+      expect(store.getState().length).toEqual(2);
     }));
 });
