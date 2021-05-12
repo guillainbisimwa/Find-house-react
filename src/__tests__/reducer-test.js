2a3558db3c8ab90a47f9a9f994aaa13071126b2d
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-12 22:18:21 +0200

    Test sign up reducer

--- src/__tests__/reducer-test.js
+++ src/__tests__/reducer-test.js
@@ -1,7 +1,26 @@
 import authentication from '../redux/reducers/AuthentificationReducer';
+import registration from '../redux/reducers/RegistrationReducer';
 
 describe('AuthentificationReducer', () => {
-  describe('Authentification Reducer', () => {
+  describe('Sign Up Reducer', () => {
+    it('should return an empty object when nothing is passed to it', () => {
+      expect(registration(undefined, {})).toEqual({});
+    });
+
+    it('should return an object request when action is passed to it', () => {
+      expect(registration([], { type: 'USERS_REGISTER_REQUEST', user: { name: 'test@me.com' } })).toEqual({ registering: true });
+    });
+
+    it('should return an empty object when success action is passed to it', () => {
+      expect(registration([], { type: 'USERS_REGISTER_SUCCESS', user: { name: 'test@me.com' } })).toEqual({ });
+    });
+
+    it('should return an empty object when failure action is passed to it', () => {
+      expect(registration([], { type: 'USERS_REGISTER_FAILURE', user: { name: 'test@me.com' } })).toEqual({});
+    });
+  });
+
+  describe('Login Reducer', () => {
     it('should return an empty object when nothing is passed to it', () => {
       expect(authentication(undefined, {})).toEqual({});
     });
