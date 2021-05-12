f96e84f2dcb33cfc1d8c37f6e8cbf2dd13329943
guillainbisimwa <guillainbisimwa@hotmail.fr>
2021-05-12 22:34:37 +0200

    Test house reducer

--- src/__tests__/reducer-test.js
+++ src/__tests__/reducer-test.js
@@ -1,4 +1,5 @@
 import authentication from '../redux/reducers/AuthentificationReducer';
+import house from '../redux/reducers/HouseReducer';
 import registration from '../redux/reducers/RegistrationReducer';
 
 describe('AuthentificationReducer', () => {
@@ -7,7 +8,7 @@ describe('AuthentificationReducer', () => {
       expect(registration(undefined, {})).toEqual({});
     });
 
-    it('should return an object request when action is passed to it', () => {
+    it('should return an object when request action is passed to it', () => {
       expect(registration([], { type: 'USERS_REGISTER_REQUEST', user: { name: 'test@me.com' } })).toEqual({ registering: true });
     });
 
@@ -25,7 +26,7 @@ describe('AuthentificationReducer', () => {
       expect(authentication(undefined, {})).toEqual({});
     });
 
-    it('should return an object request when action is passed to it', () => {
+    it('should return an object when request action is passed to it', () => {
       expect(authentication([], { type: 'USERS_LOGIN_REQUEST', user: { name: 'test@me.com' } })).toEqual({ loggingIn: true, user: { name: 'test@me.com' } });
     });
 
@@ -35,6 +36,26 @@ describe('AuthentificationReducer', () => {
 
     it('should return an empty object when failure action is passed to it', () => {
       expect(authentication([], { type: 'USERS_LOGIN_FAILURE', user: { name: 'test@me.com' } })).toEqual({});
+    });
+  });
+});
+
+describe('House Reducer', () => {
+  describe('Get All Houses Reducer', () => {
+    it('should return an empty object when nothing is passed to it', () => {
+      expect(house(undefined, {})).toEqual({});
+    });
+
+    it('should return an object (loading) when request action is passed to it', () => {
+      expect(house([], { type: 'USERS_GETALLHOUSES_REQUEST', house: { } })).toEqual({ loading: true });
+    });
+
+    it('should return an object when success action is passed to it', () => {
+      expect(house([], { type: 'USERS_GETALLHOUSES_SUCCESS', house: { } })).toEqual({ houses: { } });
+    });
+
+    it('should return an empty object when failure action is passed to it', () => {
+      expect(house([], { type: 'USERS_GETALLHOUSES_FAILURE', house: { } })).toEqual({});
     });
   });
 });
